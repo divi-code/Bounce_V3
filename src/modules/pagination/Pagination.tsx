61b5a5a26dd71be3d7c4045e2bc6a473ad0345cd
Marina Korzunova <thearnica@gmail.com>
2021-05-31 17:58:18 +1000

    pagination styles

--- src/modules/pagination/Pagination.tsx
+++ src/modules/pagination/Pagination.tsx
@@ -6,6 +6,9 @@ import { MaybeWithClassName } from "@app/helper/react/types";
 
 import { Button } from "@app/ui/button";
 
+import { RightArrow } from "@app/ui/icons/arrow-right";
+import { Body1 } from "@app/ui/typography";
+
 import styles from "./Pagination.module.scss";
 
 type PaginationType = {
@@ -27,30 +30,39 @@ export const Pagination: FC<PaginationType & MaybeWithClassName> = ({
 	return (
 		<div className={classNames(className, styles.component)}>
 			<ol className={styles.list}>
-				<li className={styles.item}>
-					<Button className={styles.button} onClick={onBack} disabled={currentPage === 0}>
+				<li>
+					<Button
+						className={styles.button}
+						onClick={onBack}
+						disabled={currentPage === 0}
+						icon={<RightArrow style={{ width: 6, height: "auto", transform: `rotate(180deg)` }} />}
+					>
 						Back
 					</Button>
 				</li>
 				{Array(1)
 					.fill(1)
 					.map((x) => (
-						<li className={styles.pagination__item} key={uid(x)}>
-							<Button className={classNames(styles.button)} disabled>
+						<li key={uid(x)}>
+							<Button className={classNames(styles.button, styles.active)} disabled>
 								{currentPage + 1}
 							</Button>
 						</li>
 					))}
-				<li className={styles.pagination__item}>
+				<li>
 					<Button
-						className={styles.pagination__button}
+						className={styles.button}
 						onClick={onNext}
 						disabled={currentPage === numberOfPages}
+						icon={<RightArrow style={{ width: 6, height: "auto" }} />}
 					>
 						Next
 					</Button>
 				</li>
 			</ol>
+			<Body1 Component="span" lighten={50}>
+				{currentPage + 1} of {numberOfPages}
+			</Body1>
 		</div>
 	);
 };
