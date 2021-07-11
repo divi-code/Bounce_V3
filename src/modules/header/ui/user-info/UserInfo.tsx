57e9975e6b078790f4a770fe2c634625a7d31c0b
Marina Korzunova <thearnica@gmail.com>
2021-07-11 10:00:37 +1000

    add account links

--- src/modules/header/ui/user-info/UserInfo.tsx
+++ src/modules/header/ui/user-info/UserInfo.tsx
@@ -1,13 +1,13 @@
 import { useWeb3React } from "@web3-react/core";
 import classNames from "classnames";
-import { useCallback, useEffect, useState } from "react";
-import CopyToClipboard from "react-copy-to-clipboard";
 
+import React, { useCallback, useEffect, useState } from "react";
+
+import { ACCOUNT_PATH } from "@app/const/const";
 import { MaybeWithClassName } from "@app/helper/react/types";
 
 import { CopyAddress } from "@app/modules/copy-to-clipboard";
-import { Button } from "@app/ui/button";
-import { Copy } from "@app/ui/icons/copy";
+import { Button, NavLink } from "@app/ui/button";
 import { Exit } from "@app/ui/icons/exit";
 import { ShortLogo } from "@app/ui/icons/short-logo";
 import { fromWei } from "@app/utils/bn/wei";
@@ -15,16 +15,18 @@ import { walletConversion } from "@app/utils/convertWallet";
 
 import { getEthBalance } from "@app/web3/api/bounce/erc";
 import { useWalletConnection } from "@app/web3/hooks/use-connection";
-import { useWeb3 } from "@app/web3/hooks/use-web3";
+import { useChainId, useWeb3 } from "@app/web3/hooks/use-web3";
+
+import { WEB3_NETWORKS } from "@app/web3/networks/const";
 
 import styles from "./UserInfo.module.scss";
 
 import type { FC } from "react";
 
 type UserInfoType = {
-	balance?: string;
+	balance: string;
 	address: string;
-	ethBalance: string;
+	token: string;
 	onLogout(): void;
 };
 
@@ -34,23 +36,13 @@ export const UserInfoView: FC<ComponentType> = ({
 	className,
 	balance,
 	address,
-	ethBalance,
+	token,
 	onLogout,
 }) => {
-	const convertedBalance = balance === undefined ? 0 : parseFloat(balance);
-
-	const [isCopy, setCopy] = useState<boolean>(false);
-
-	useEffect(() => {
-		if (isCopy) {
-			setTimeout(() => setCopy(false), 1000);
-		}
-	}, [isCopy]);
-
 	return (
 		<div className={classNames(className, styles.component)}>
 			<span className={styles.balance}>
-				{convertedBalance.toFixed(2)} {convertedBalance > 0 ? "Auctions" : "Auction"}
+				{balance} {token}
 			</span>
 			<span className={styles.address}>{walletConversion(address)}</span>
 			<div className={styles.wrapper}>
@@ -60,7 +52,17 @@ export const UserInfoView: FC<ComponentType> = ({
 				<div className={styles.dropdown}>
 					<div className={styles.info}>
 						<CopyAddress className={styles.copyAddress} address={address} />
-						<span className={styles.ethBalance}>{ethBalance} ETH</span>
+					</div>
+					<div className={styles.links}>
+						<NavLink className={styles.link} href={`${ACCOUNT_PATH}`} weight="bold" exact>
+							My Auctions
+						</NavLink>
+						<NavLink className={styles.link} href={`${ACCOUNT_PATH}/otc`} weight="bold">
+							My OTC
+						</NavLink>
+						<NavLink className={styles.link} href={`${ACCOUNT_PATH}/activity`} weight="bold">
+							Activities
+						</NavLink>
 					</div>
 					<Button
 						className={styles.logout}
@@ -80,6 +82,7 @@ export const UserInfo = () => {
 
 	const { account } = useWeb3React();
 	const web3 = useWeb3();
+	const chainId = useChainId();
 
 	const updateData = useCallback(async () => {
 		getEthBalance(web3, account).then((b) => setBalance(b));
@@ -100,7 +103,8 @@ export const UserInfo = () => {
 	return (
 		<UserInfoView
 			address={account}
-			ethBalance={parseFloat(fromWei(balance, 18).toFixed(4, 1)).toString()}
+			token={chainId === WEB3_NETWORKS.BINANCE ? "BNS" : "ETH"}
+			balance={parseFloat(fromWei(balance, 18).toFixed(4, 1)).toString()}
 			onLogout={disconnectWallet}
 		/>
 	);
