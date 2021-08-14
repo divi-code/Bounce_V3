e7b01d848aed89a85d557a55b13f828360076ed8
huangxuelun <15216222032@163.com>
2021-08-14 14:57:38 +0800

    fix: add bnb basetoken

--- src/web3/api/eth/token/token.ts
+++ src/web3/api/eth/token/token.ts
@@ -1,22 +1,41 @@
 import { TokenInfo } from "@uniswap/token-lists";
 
+import BNB from "./assets/bnb.svg";
 import ETHER from "./assets/eth.svg";
+
 import { makeToken } from "./utils";
 
 import { WETH9 } from "./weth9";
 
 const tokenCache: Record<number, TokenInfo> = {};
 
+const getBaseToken = (chainId: number) => {
+	switch (chainId) {
+		case 56:
+			return makeToken(
+				chainId,
+				"0x0000000000000000000000000000000000000000",
+				18,
+				"BNB",
+				"Binance",
+				BNB
+			);
+
+		default:
+			return makeToken(
+				chainId,
+				"0x0000000000000000000000000000000000000000",
+				18,
+				"ETH",
+				"Ether",
+				ETHER
+			);
+	}
+};
+
 export const getEtherChain = (chainId: number): TokenInfo => {
 	if (!tokenCache[chainId]) {
-		tokenCache[chainId] = makeToken(
-			chainId,
-			"0x0000000000000000000000000000000000000000",
-			18,
-			"ETH",
-			"Ether",
-			ETHER
-		);
+		tokenCache[chainId] = getBaseToken(chainId);
 	}
 
 	return tokenCache[chainId];
